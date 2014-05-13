bfc264fca2f4da7e18561e58251d65d467e60b1d
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2014-05-13 23:26:11 +0300

    run docker stuff

--- local_scripts/docker_run.sh
+++ local_scripts/docker_run.sh
@@ -1,28 +1,32 @@
-#IMAGE_NAME="reslocal/yocto"
 IMAGE_NAME=$1
-#HOST_FOLDER="${HOME}/yocto-image"
-#IMAGE_FOLDER="/home/genius/yocto"
-# this is a hack against permission denied
-# there is still a problem with yocto when it populates the rootfs
-#if [ ! -d ${HOST_FOLDER} ];
-#then
-#     echo "${HOST_FOLDER} does not exist, creating it"
-#     mkdir -p ${HOST_FOLDER}
-#     chmod a+w ${HOST_FOLDER}
-#else
-#     echo "${HOST_FOLDER} exists"
-#fi
-#docker run -i -t reslocal/yocto /bin/bash
-#echo "+ ID=\$(docker run -p 127.0.0.1::22 -i -t -d ${IMAGE_NAME} /bin/bash)"
-#ID=$(docker run -p 127.0.0.1::22 -i -t -d ${IMAGE_NAME} /bin/bash)
-#echo "+ ID=\$(docker run -i -t -d ${IMAGE_NAME} /bin/bash)"
-#ID=$(docker run -i -t -d ${IMAGE_NAME} /bin/bash)
-#echo "+ ID=\$(docker run -i -t -d -v ${HOST_FOLDER}:${IMAGE_FOLDER}:rw ${IMAGE_NAME} /bin/bash)"
-#ID=$(docker run -i -t -d -v ${HOST_FOLDER}:${IMAGE_FOLDER}:rw ${IMAGE_NAME} /bin/bash)
-echo "+ ID=\$(docker run -i -t -d ${IMAGE_NAME} /bin/bash)"
-ID=$(docker run -i -t -d ${IMAGE_NAME} /bin/bash)
-#echo "+ IPAddress: $(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${ID})"
-# ssh does not work for now
-#echo "+ ssh to: $(docker port ${ID} 22)"
+NETWORK_INTERFACE=$2
+
+if [ $# -lt 2 ];
+then
+    echo "+ $0: Too few arguments!"
+    echo "+ use something like:"
+    echo "+ $0 <docker image> <network interface>" 
+    echo "+ $0 reslocal/yocto br0"
+    exit
+fi
+
+# enable TUN device (for qemu)
+echo "+ sudo modprobe tun"
+sudo modprobe tun
+
+# run the image
+#echo "+ ID=\$(docker run -i -t -d -p 22 --privileged ${IMAGE_NAME} /bin/bash)"
+#ID=$(docker run -i -t -d -p 22 --privileged ${IMAGE_NAME} /bin/bash)
+
+echo "+ ID=\$(docker run -t -i -d -p 22 --privileged ${IMAGE_NAME} /sbin/my_init -- bash -l)"
+ID=$(docker run -t -i -d -p 22 --privileged ${IMAGE_NAME} /sbin/my_init -- bash -l)
+
+# ssh stuff:
+PORT=$(docker port ${ID} 22 | awk -F':' '{ print $2 }')
+IPADDR=$(ifconfig ${NETWORK_INTERFACE} | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
+echo "+ ssh to the container like this:"
+echo "ssh -X genius@${IPADDR} -p ${PORT}"
+
+# let's attach to it:
 echo "+ docker attach ${ID}"
 docker attach ${ID}
