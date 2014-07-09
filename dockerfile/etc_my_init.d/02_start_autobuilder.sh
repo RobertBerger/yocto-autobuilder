6da40e1f81fc5d3d5a3dab708057bf286d6268a2
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2014-07-09 23:43:43 +0300

    fix permissions

--- dockerfile/etc_my_init.d/02_start_autobuilder.sh
+++ dockerfile/etc_my_init.d/02_start_autobuilder.sh
@@ -4,5 +4,12 @@ set -e
 if [[ -d /home/genius/test/yocto-autobuilder ]]; then
       echo "starting yocto-autobuilder..."
       (su genius -c "cd /home/genius/test/yocto-autobuilder ; ./yocto-stop-autobuilder both; . ./yocto-autobuilder-setup; ./yocto-start-autobuilder both")
-      exit 0
 fi
+
+if [[ $(stat -c %U /home/genius/test) == "root" ]]; then
+    echo "fixing pemissions"
+    sudo chown genius:genius /home/genius/test
+fi
+
+exit 0
+
