a10421638caeaeec2edf2a258191666d79d8fde3
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2014-07-10 21:43:08 +0300

    permissions hack

--- dockerfile/etc_my_init.d/02_start_autobuilder.sh
+++ dockerfile/etc_my_init.d/02_start_autobuilder.sh
@@ -6,15 +6,15 @@ if [[ -d /home/genius/test/yocto-autobuilder ]]; then
       (su genius -c "cd /home/genius/test/yocto-autobuilder ; ./yocto-stop-autobuilder both; . ./yocto-autobuilder-setup; ./yocto-start-autobuilder both")
 fi
 
-#if [[ $(stat -c %U /home/genius) == "root" ]]; then
-#    echo "fixing pemissions"
-#    sudo chown genius:genius /home/genius
-#fi
+if [[ $(stat -c %U /home/genius) == "root" ]]; then
+    echo "fixing pemissions"
+    sudo chown genius:genius /home/genius
+fi
 
-#if [[ $(stat -c %U /home/genius/test) == "root" ]]; then
-#    echo "fixing pemissions"
-#    sudo chown genius:genius /home/genius/test
-#fi
+if [[ $(stat -c %U /home/genius/test) == "root" ]]; then
+    echo "fixing pemissions"
+    sudo chown genius:genius /home/genius/test
+fi
 
 exit 0
 
