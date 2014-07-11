724b45a578217507b89c16f373d3216a2d2b4aac
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2014-07-11 17:55:16 +0300

    I think it's enough to call once tightvncserver

--- dockerfile/etc_my_init.d/02_start_autobuilder.sh
+++ dockerfile/etc_my_init.d/02_start_autobuilder.sh
@@ -4,6 +4,7 @@ set -e
 if [[ -d /home/genius/test/yocto-autobuilder ]]; then
       echo "starting yocto-autobuilder..."
       (su genius -c "cd /home/genius/test/yocto-autobuilder ; ./yocto-stop-autobuilder both; . ./yocto-autobuilder-setup; ./yocto-start-autobuilder both")
+      (su genius -c "tightvncserver")
 fi
 
 if [[ $(stat -c %U /home/genius) == "root" ]]; then
