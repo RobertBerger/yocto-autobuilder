ace2a55f3db23a5cd86f0aa4227805b1bef41792
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2014-07-12 09:02:43 +0300

    fix tighvnc server once

--- dockerfile/etc_my_init.d/02_start_autobuilder.sh
+++ dockerfile/etc_my_init.d/02_start_autobuilder.sh
@@ -4,7 +4,6 @@ set -e
 if [[ -d /home/genius/test/yocto-autobuilder ]]; then
       echo "starting yocto-autobuilder..."
       (su genius -c "cd /home/genius/test/yocto-autobuilder ; ./yocto-stop-autobuilder both; . ./yocto-autobuilder-setup; ./yocto-start-autobuilder both")
-      (su genius -c "tightvncserver")
 fi
 
 if [[ $(stat -c %U /home/genius) == "root" ]]; then
@@ -15,6 +14,13 @@ fi
 if [[ $(stat -c %U /home/genius/test) == "root" ]]; then
     echo "fixing pemissions"
     sudo chown genius:genius /home/genius/test
+fi
+
+if [ $(ps aux | grep -e 'Xtightvnc' | grep -v grep | wc -l | tr -s "\n") -eq 0 ]; then
+    echo "Xtightvnc Stopped -  starting it"
+    (su genius -c "tightvncserver")
+else
+    echo "Xtightvnc already running"
 fi
 
 exit 0
