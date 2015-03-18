d4cc6031d3850a02b7469f4028ff7f82173e14bf
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2015-03-19 00:58:17 +0200

    delete X locks

--- dockerfile/etc_my_init.d/02_start_autobuilder.sh
+++ dockerfile/etc_my_init.d/02_start_autobuilder.sh
@@ -16,6 +16,10 @@ if [[ $(stat -c %U /home/genius/test) == "root" ]]; then
     sudo chown genius:genius /home/genius/test
 fi
 
+# Delete X locks
+(su genius -c "rm -f /tmp/.X*-lock")
+(su genius -c "rm -f /tmp/.X11-unix/*")
+
 if [ $(ps aux | grep -e 'Xtightvnc' | grep -v grep | wc -l | tr -s "\n") -eq 0 ]; then
     echo "Xtightvnc Stopped -  starting it"
     (su genius -c "tightvncserver")
