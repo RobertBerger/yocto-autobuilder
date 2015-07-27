76028078fb27c3eeb9dbfd7ddc6c9e7a4f0995f9
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2015-07-28 00:56:43 +0300

    attempt to have autobuilder on volume

--- dockerfile/etc_my_init.d/02_start_autobuilder.sh
+++ dockerfile/etc_my_init.d/02_start_autobuilder.sh
@@ -1,31 +1,55 @@
 #!/bin/bash
 set -e
 
-if [[ -d /home/genius/test/yocto-autobuilder ]]; then
-      echo "starting yocto-autobuilder..."
-      (su genius -c "cd /home/genius/test/yocto-autobuilder ; ./yocto-stop-autobuilder both; . ./yocto-autobuilder-setup; ./yocto-start-autobuilder both")
-fi
+if grep -qs '/tmp/yocto-autobuilder' /proc/mounts; then
+    echo "/tmp/yocto-autobuilder is mounted."
 
-if [[ $(stat -c %U /home/genius) == "root" ]]; then
-    echo "fixing pemissions"
-    sudo chown genius:genius /home/genius
-fi
+    if [[ -d /home/genius/test/yocto-autobuilder ]]; then
 
-if [[ $(stat -c %U /home/genius/test) == "root" ]]; then
-    echo "fixing pemissions"
-    sudo chown genius:genius /home/genius/test
-fi
+      if [[ $(stat -c %U /home/genius) == "root" ]]; then
+        echo "fixing pemissions"
+        sudo chown genius:genius /home/genius
+      fi
 
-# Delete X locks
-(su genius -c "rm -f /tmp/.X*-lock")
-(su genius -c "rm -f /tmp/.X11-unix/*")
+      if [[ $(stat -c %U /home/genius/test) == "root" ]]; then
+        echo "fixing pemissions"
+        sudo chown genius:genius /home/genius/test
+      fi
 
-if [ $(ps aux | grep -e 'Xtightvnc' | grep -v grep | wc -l | tr -s "\n") -eq 0 ]; then
-    echo "Xtightvnc Stopped -  starting it"
-    (su genius -c "tightvncserver")
+      rsync -avp /home/genius/test/yocto-autobuilder /tmp/yocto-autobuilder
+      mv /home/genius/test/yocto-autobuilder /home/genius/test/yocto-autobuilder.ori
+    else
+      echo "/home/genius/test/yocto-autobuilder does not exist anymore"
+    fi
 else
-    echo "Xtightvnc already running"
+    echo "/tmp/yocto-autobuilder is not mounted."
+    exit 0 
 fi
 
+  if [[ -d /tmp/yocto-autobuilder/yocto-autobuilder ]]; then
+        echo "starting yocto-autobuilder..."
+        (su genius -c "cd /tmp/yocto-autobuilder/yocto-autobuilder ; ./yocto-stop-autobuilder both; . ./yocto-autobuilder-setup; ./yocto-start-autobuilder both")
+  fi
+
+# if [[ $(stat -c %U /home/genius) == "root" ]]; then
+#     echo "fixing pemissions"
+#     sudo chown genius:genius /home/genius
+# fi
+
+# if [[ $(stat -c %U /home/genius/test) == "root" ]]; then
+#     echo "fixing pemissions"
+#     sudo chown genius:genius /home/genius/test
+# fi
+
+  # Delete X locks
+  (su genius -c "rm -f /tmp/.X*-lock")
+  (su genius -c "rm -f /tmp/.X11-unix/*")
+
+  if [ $(ps aux | grep -e 'Xtightvnc' | grep -v grep | wc -l | tr -s "\n") -eq 0 ]; then
+      echo "Xtightvnc Stopped -  starting it"
+      (su genius -c "tightvncserver")
+  else
+      echo "Xtightvnc already running"
+  fi
 exit 0
 
